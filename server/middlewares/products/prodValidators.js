160a86674356e6d8ffe9f7fe64995db2b65995c4
Abeer <abeer.technology@gmail.com>
2021-09-24 12:11:26 +0600

    added product info validation

---
+++ server/middlewares/products/prodValidators.js
@@ -0,0 +1,56 @@
+const { check, validationResult } = require("express-validator");
+const { unlink } = require("fs");
+const path = require("path");
+
+const Product = require("../../models/Product");
+
+const prodValidator = [
+  check("prodName")
+    .isLength({ min: 1 })
+    .withMessage("product name is required!")
+    .isAlpha("en-US", { ignore: " -" })
+    .withMessage("Name must not contain anything other than alphabet.")
+    .trim()
+    .custom(async (value) => {
+      try {
+        const product = await Product.findOne({ prodName: value });
+        if (product) {
+          throw Error("This product is already in stock! 🧨");
+        }
+      } catch (err) {
+        throw Error(err.message);
+      }
+    }),
+  check("quantity").isNumeric(),
+  check("price").isNumeric(),
+  check("stock").isNumeric(),
+];
+
+const prodValidationResult = (req, res, next) => {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+  if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    // remove uploaded files
+    if (req.file) {
+      const { filename } = req.file;
+      unlink(
+        path.join(__dirname, `/../../public/uploads/${filename}`),
+        (err) => {
+          if (err) console.log(err);
+        }
+      );
+    }
+
+    // response the errors
+    res.status(500).json({
+      errors: mappedErrors,
+    });
+  }
+};
+
+module.exports = {
+  prodValidator,
+  prodValidationResult,
+};
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -2,11 +2,21 @@ const express = require("express");
 
 const { getProducts, uploadProduct } = require("../controllers/prodController");
 const upload = require("../middlewares/products/picUpload");
+const {
+  prodValidator,
+  prodValidationResult,
+} = require("../middlewares/products/prodValidators");
 
 const router = express.Router();
 
 router.get("/", getProducts);
 
-router.post("/upload", upload.single("prodImage"), uploadProduct);
+router.post(
+  "/upload",
+  upload.single("prodImage"),
+  prodValidator,
+  prodValidationResult,
+  uploadProduct
+);
 
 module.exports = router;
