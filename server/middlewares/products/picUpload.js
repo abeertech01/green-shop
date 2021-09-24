27a780c5639d1850933edf5cd7cf197090a7c810
Abeer <abeer.technology@gmail.com>
2021-09-24 10:37:04 +0600

    photo upload worked

---
+++ server/middlewares/products/picUpload.js
@@ -0,0 +1,39 @@
+const multer = require("multer");
+const path = require("path");
+
+const UPLOADS_FOLDER = `${__dirname}/../../public/uploads`;
+const allowed_file_types = ["image/jpeg", "image/jpg", "image/png"];
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOADS_FOLDER);
+  },
+  filename: (req, file, cb) => {
+    const fileExt = path.extname(file.originalname);
+    const fileName =
+      file.originalname
+        .replace(fileExt, "")
+        .toLowerCase()
+        .split(" ")
+        .join("-") +
+      "-" +
+      Date.now();
+    cb(null, fileName + fileExt);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 12000000,
+  },
+  fileFilter: (req, file, cb) => {
+    if (allowed_file_types.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only .jpeg, .jpg and .png files are allowed!"));
+    }
+  },
+});
+
+module.exports = upload;
--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const { getProducts, uploadProduct } = require("../controllers/prodController");
+const upload = require("../middlewares/products/picUpload");
 
 const router = express.Router();
 
 router.get("/", getProducts);
 
-router.post("/upload", uploadProduct);
+router.post("/upload", upload.single("photo"), uploadProduct);
 
 module.exports = router;
